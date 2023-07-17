b4a922e41ab4edeb964ef8245e23251d8274cf08
NickCode <Garfild.odi@mail.ru>
2023-07-17 18:31:44 +0300

    Использовать в продакшене

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,15 +1,11 @@
-const menu_links = Array.from(document.getElementsByClassName("menu__link"));
+let menuLink = document.querySelectorAll('.menu__link');
 
-for (let i = 0; i < menu_links.length; i++) {
-  const link = menu_links[i];
-
-  link.addEventListener("click", function (event) {
-    const parent = link.closest('.menu__item');
-    const submenu = parent.querySelector('.menu_sub');
-
-    if (submenu) {
-      event.preventDefault();
-      parent.classList.toggle('menu_active');
+for(let link of menuLink){
+    link.onclick = function(e){
+        const menuSub = link.parentElement.querySelector('.menu_sub');
+        if(menuSub) {
+            e.preventDefault();
+            menuSub.classList.toggle('menu_active');
+        }
     }
-  });
 }
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -2,37 +2,6 @@
 const left = document.querySelector('.slider__arrow_prev');
 const slider_item = Array.from(document.querySelectorAll('.slider__item'));
 
-/* left.onclick = function clickLeft () {
-    console.log("Нажат левый слайдер");
-}
-
-const allSlides = Array.from(document.querySelectorAll(".slider__item")); 
-let currentSlide = allSlides.findIndex(el => el.classList.contains('slider__item_active'))
-// Кнопки
-const slidePrev = document.querySelector('.slider__arrow_prev');
-const slideNext = document.querySelector('.slider__arrow_next');
-
-function nextSlide() {
-    goToSlide(currentSlide + 1);
-}
-
-function previousSlide() {
-    goToSlide(currentSlide - 1);
-}
-
-function goToSlide(n) {
-    allSlides[currentSlide].classList.remove('slider__item_active')    // удаляем актив у предидущего
-    currentSlide = (n + allSlides.length) % allSlides.length;       // изменяем номер слайда
-    allSlides[currentSlide].classList.add('slider__item_active')    // добавляем актив новому
-}
-//      Активируем кнопки по клику
-slideNext.onclick = function() {
-    nextSlide();
-};
-slidePrev.onclick = function() {
-    previousSlide();
-}; */
-
 const slider_items = Array.from(document.querySelectorAll('.slider__item'));
 let activeIndex = 0;
 
