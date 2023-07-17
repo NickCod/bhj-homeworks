e0fa1bd98a688a326e279bfda1bb5d80c4811faf
NickCode <Garfild.odi@mail.ru>
2023-07-17 19:13:05 +0300

    Использовать в продакшене

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,11 +1,19 @@
-let menuLink = document.querySelectorAll('.menu__link');
+let menuLinks = document.querySelectorAll('.menu__link');
+let activeMenu = null;
 
-for(let link of menuLink){
-    link.onclick = function(e){
-        const menuSub = link.parentElement.querySelector('.menu_sub');
-        if(menuSub) {
-            e.preventDefault();
-            menuSub.classList.toggle('menu_active');
-        }
+for (let link of menuLinks) {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    
+    const menuSub = link.parentElement.querySelector('.menu_sub');
+    
+    if (menuSub) {
+      if (activeMenu && activeMenu !== menuSub) {
+        activeMenu.classList.remove('menu_active');
+      }
+      
+      menuSub.classList.toggle('menu_active');
+      activeMenu = menuSub;
     }
+  });
 }
