dc53fc46390377c69c58412f9164e102721a5ecc
unknown <Garfild.odi@mail.ru>
2023-07-27 20:56:04 +0300

    Непонятный чат

--- html-forms/chat/task.js
+++ html-forms/chat/task.js
@@ -1,5 +1,40 @@
 let sideWid = document.querySelector('.chat-widget');
+let chatText = document.getElementById('chat-widget__input');
+let messages = document.querySelector('.chat-widget__messages');
 
 sideWid.addEventListener('click', ()=> {
 sideWid.classList.toggle('chat-widget_active')
 });
+
+chatText.addEventListener('click', (e)=> {
+e.stopPropagation();
+})
+
+function CheckKey () {
+document.addEventListener('keydown', (event)=>{
+ let key = event.key;
+ console.log(key);
+ DelayMesage(key);
+}) 
+}
+
+function DelayMesage(key) {
+ let myMessage = chatText.value.trim();
+    if (key === "Enter") {
+      console.log('Enter was pressed!');
+      messages.innerHTML += myMessage;
+       setTimeout(()=>{
+messages.innerHTML +=
+        `
+        <div class="message">
+          <div class="message__time">09:21</div>
+          <div class="message__text">
+            Добрый день, мы ещё не проснулись. Позвоните через 10 лет
+          </div>
+        </div>
+        `;
+       },1000);
+    }
+  }
+  
+  CheckKey();  
