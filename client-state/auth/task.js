9208734c972489ff6bed885b78c4a0cdaecf5233
Никита Селютин <Garfild.odi@mail.ru>
2023-08-07 12:48:06 +0300

    Доделать аутендификацию

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,38 +1,43 @@
-const form = document.getElementById('signin__form');
-const formData = new FormData(form);
-
-let xhr = new XMLHttpRequest();
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  let login = document.querySelector('.control[name="login"]').value;
-  let password = document.querySelector('.control[name="password"]').value;
-   let storeId = localStorage.getItem('id');
-
-  if (login === "demo" && password === "demo") {
-    if(!storeId){
-    let id = document.getElementById('user_id').textContent;
-     let idStorage = localStorage.setItem('id', id);
-    let welcome = document.getElementById('welcome');
-    welcome.classList.add('welcome_active');
-    welcome.textContent = "Добро пожаловать, пользователь #" + idStorage();
-    [login, password].forEach((element) => element.value = '');
-    }
-  } else {
-    alert('Неверный логин/пароль')
-  }
-
-  xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
-  xhr.send(formData);
-});
-
-window.onload = () => {
-    let storedId = localStorage.getItem('id');
-    if (storedId) {
-      let welcome = document.getElementById('welcome');
-      welcome.classList.add('welcome_active');
-      welcome.textContent = "Добро пожаловать, пользователь #" + storedId;
-    }
-  };
-
-
+(() => {
+  const authForm = document.getElementById('signin__form');
   
+  authForm.addEventListener('submit', function(event) {
+    console.log('Submit event fired');
+      event.preventDefault();
+      const xhr = new XMLHttpRequest();
+      xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
+      xhr.onreadystatechange = () => {
+          if (xhr.status >= 400) {
+              console.warn('Произошла ошибка при передачи данных!');
+              return;
+          }
+          if (xhr.readyState === xhr.DONE) {
+              const response = JSON.parse(xhr.response);
+              if (response.success) {
+                  showWelcome(response.user_id);
+              } else {
+                  alert('Неверный логин/пароль');
+              }
+          }
+      };
+      
+      const formData = new FormData(this);
+      xhr.send(formData);
+      authForm.reset();
+  });
+  
+  window.addEventListener('load', () => {
+      const user_id = JSON.parse(localStorage.getItem('user_id'));
+      if (user_id) {
+          showWelcome(user_id);
+      }
+  });
+  
+  function showWelcome(id) {
+      authForm.parentElement.classList.remove('signin_active');
+      const welcomeBlock = document.getElementById('welcome');
+      welcomeBlock.classList.add('welcome_active');
+      welcomeBlock.innerHTML = `Добро пожаловать, пользователь #<span id="user_id">${id}</span>`;
+  }
+  
+  });
--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,20 +1,20 @@
 const modal = document.getElementById('subscribe-modal');
 
-function showModal () {
-    modal.style.display ="block";
-   }
-     const storeModal = localStorage.getItem('modal')
+function showModal() {
+    modal.style.display = "block";
+}
 
-     if (!storeModal){
-      showModal();
-       localStorage.setItem('modal', 'shown');
-     } else {
-      const closeSModal = document.querySelector('.modal__close');
-      closeSModal.addEventListener('click', ()=> {
-        modal.style.display ="none";
-     })
-    }
+const storeModal = localStorage.getItem('modal');
 
+if (!storeModal) {
+    showModal();
+} else if (storeModal === 'shown') {
+    const closeSModal = document.querySelector('.modal__close');
+    closeSModal.addEventListener('click', () => {
+        modal.style.display = "none";
+        localStorage.setItem('modal', 'closed');
+    });
+}
 
 
  
